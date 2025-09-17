bb6774891bccf2f68cc30b8b9cf6d284d0802002
ashuOO7 <ashjnavj@gmail.com>
2025-09-18 04:07:21 +0530

    getting user lists on server

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const { ApolloServer } = require("apollo-server");
 const { typeDefs } = require("./schema/type-defs");
-const { resolvers } = require("./schema/type-defs");
+const { resolvers } = require("./schema/resolvers");
 
 
 const server = new ApolloServer({ typeDefs, resolvers });
 // resolvers are the fn which deals with the data
 
-server.listen().then( ({url}) => {
-    console.log("API is running correctly from the URL===${url}==:", )
-})
+server.listen({ port: 4000 }).then(({ url }) => {
+  console.log(`API is running correctly at ${url}`);
+});
--- schema/resolvers.js
+++ schema/resolvers.js
@@ -1,22 +1,12 @@
 const { UserList } = require("../TestData");
 
 const resolvers = {
-    // Query: {
-    //     // USER RESOLVERS
-    //     users: () => {
-    //     return UserList;
-    //     },
-    //     user: (parent, args) => {
-    //     const id = args.id;
-    //     const user = _.find(UserList, { id: Number(id) });
-    //     return user;
-    //     },
-    // }
 
-     Query: {
-        // USER RESOLVERS
-        users () {
-            return UserList;
-        },
+  Query: {
+    users: () => {
+      return UserList; 
     },
+  },
 };
+
+module.exports = { resolvers };
