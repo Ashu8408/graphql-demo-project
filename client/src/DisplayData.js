abf7502ce1011cd582813461df5a59719c78bdc7
ashuOO7 <ashjnavj@gmail.com>
2025-09-21 15:06:14 +0530

    fetching more data

--- client/src/DisplayData.js
+++ client/src/DisplayData.js
@@ -9,24 +9,47 @@ const QUERY_ALL_USERS = gql`
       name
       age
       username
+      nationality
     }
   }
+`;
+const QUERY_ALL_COMPANY = gql`
+ query GetAllCompany {
+   companies{
+     id
+     name
+     dateOfJoining
+     isWorking
+   }
+ }
 `;
 
 function DisplayData() {
   const { loading, error, data } = useQuery(QUERY_ALL_USERS);
+  const { data: companyData } = useQuery( QUERY_ALL_COMPANY);
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error: {error.message}</p>;
+  
+  if (companyData){
+    console.log(companyData)
+  }
 
   return (
     <div>
       <h2>Display Data Content</h2>
+      
       {data.users.map((user) => (
         <div key={user.id}>
-          <h3>{user.name} ({user.username}) — {user.age} years old</h3>
+          <h3>{user.name} ({user.username}) — {user.age} years old is from {user.nationality}</h3>
           {/* <h3> ({user.username})</h3>
           <h3>{user.age}</h3> */}
+        </div>
+      ))}
+      <br/>
+      {companyData.companies.map((company) => (
+        <div key={company.id}>
+          <h3> Company Name: {company.name}, Date of joining: ({company.dateOfJoining}) isWorking: {company.isWorking}</h3>
         </div>
       ))}
     </div>
